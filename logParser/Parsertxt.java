44c7fb2d3136b7d7e4c227f860393d8ea7dbeeaa
Abhijit Bhagavati <abhijit.bhagavati1111@gmail.com>
2024-09-29 21:08:55 +0530

    Added some response Time parsing logic

--- logParser/Parsertxt.java
+++ logParser/Parsertxt.java
@@ -1,10 +1,16 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 public class Parsertxt{
 
@@ -26,50 +32,70 @@ public class Parsertxt{
 
     }    
 
-    Map <String,String> ReqRes= new HashMap<>(); 
     public void parseLine(String Line){
-
+        
         String parts [] =Line.split(" ",6);
-
+        
         String DTTM=parts[0]+ " " +parts[1];
         String LogLevel=parts[2];
         String func=parts[3];
         String message=parts[5];
-
+        
         switch(LogLevel){
             case "INFO": 
-                processInfo(message,func);
-                break;
+            processInfo(message,func,DTTM);
+            break;
             
             case "ERROR": 
-                processError(message,func);
-                break;
+            processError(message,func,DTTM);
+            break;
             
             case "DEBUG":
-                processDebug(message,func);
+            processDebug(message,func,DTTM);
             default:
-                break;
+            break;
         }
-
-        
-        
-        
     }
-    public void processInfo(String message,String func){
+        
+        
+        
+        
+    Map <String,List<String>> ReqRes= new HashMap<>(); 
+    public void processInfo(String message,String func,String DTTM){
 
         if(func.equalsIgnoreCase("api_call")){
             String div[]= message.split(" ",3);
-            if(div[0].equalsIgnoreCase("get")){
-                
-            }
+            
+                String apiP[]=div[2].split(",");
+                String api=apiP[0];
+                String requestId=apiP[2];
+                if(ReqRes.get(api)!=null)
+                {
+                    calcTime(api,DTTM);
+                }else{
+                    ReqRes.put(api,Arrays.asList(requestId,DTTM));
+                }   
+
+            
         }
     }
+    public void calcTime(String api,String DTTM){
+        String reqTime=ReqRes.get(api).get(1);
+        String resTime= DTTM;
+        DateTimeFormatter formatter= DateTimeFormatter.ofPattern("YYYY-MM-DD HH:MM:SS");
+        LocalDateTime reqTimeA=LocalDateTime.parse(reqTime, formatter);
+        LocalDateTime resTimeA=LocalDateTime.parse(resTime,formatter);
+        Duration responseTime=Duration.between(resTimeA,reqTimeA);
 
-    public void processError(String message,String func){
+        StringBuilder diff=new StringBuilder();
+        diff.append(responseTime.ofMinutes(0));
+    }
+
+    public void processError(String message,String func,String DTTM){
         
     }
     
-    public void processDebug(String message,String func){
+    public void processDebug(String message,String func,String DTTM){
 
     }
     
